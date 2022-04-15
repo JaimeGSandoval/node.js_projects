bb0e734b93fddf62c53a6718c116b5571e1d131b
jaimegsandoval <jaime.devs@gmail.com>
2022-04-14 20:54:49 -0700

    remove console logs

--- natours/app.js
+++ natours/app.js
@@ -1,36 +1,31 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
-const morgan = require('morgan');
 
 // ROUTERS
 const tourRouter = require('./routes/toursRoutes');
 const usersRouter = require('./routes/usersRoutes');
 
-console.log(process.env.NODE_ENV);
-
 // MIDDLEWARE
 if (process.env.NODE_ENV === 'development') {
-  console.log('bankai');
   app.use(morgan('dev')); // logger of req data
 }
 app.use(express.json()); // req body parser
 app.use(express.static(`${__dirname}/public`)); // serve static files
 
-app.use((req, res, next) => {
-  // console.log('Hello from the middleware');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware');
+//   next();
+// });
 
-app.use((req, res, next) => {
-  req.requestTime = new Date().toISOString();
-  next();
-});
+// app.use((req, res, next) => {
+//   req.requestTime = new Date().toISOString();
+//   next();
+// });
 
 // ROUTES
 app.use('/api/v1/tours', tourRouter); // mounting a router
 app.use('/api/v1/users', usersRouter); // mounting a router
-
-// SERVER
 
 module.exports = app;
