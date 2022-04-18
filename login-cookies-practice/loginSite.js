a11d6579addb07659a47ab18a89316ed3051243e
jaimegsandoval <jaime.devs@gmail.com>
2022-04-18 15:08:34 -0700

    initial commit

---
+++ login-cookies-practice/loginSite.js
@@ -0,0 +1,25 @@
+require('dotenv/config');
+const express = require('express');
+const helmet = require('helmet');
+const path = require('path');
+
+const app = express();
+
+app.use(helmet());
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views'));
+
+app.get('/', (req, res, next) => {
+  res.send('Sanity check');
+});
+
+app.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+const port = process.env.PORT;
+app.listen(port || 3000, () => console.log(`I'm listening on port ${port}...`));
