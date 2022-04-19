a86f803d514634acad0f0d657d7b5c59a958e4ee
jaimegsandoval <jaime.devs@gmail.com>
2022-04-18 17:13:50 -0700

    Add logout route and redirect to login page

--- login-cookies-practice/loginSite.js
+++ login-cookies-practice/loginSite.js
@@ -2,13 +2,15 @@ require('dotenv/config');
 const express = require('express');
 const helmet = require('helmet');
 const path = require('path');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 app.use(helmet());
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false })); // gets form data and places it on req.body
+app.use(cookieParser());
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
@@ -19,10 +21,6 @@ app.get('/', (req, res, next) => {
 
 app.get('/login', (req, res, next) => {
   res.render('login');
-});
-
-app.get('/welcome', (req, res, next) => {
-  res.send('Welcome');
 });
 
 app.post('/process_login', (req, res, next) => {
@@ -36,6 +34,17 @@ app.post('/process_login', (req, res, next) => {
     res.direct('/login?message=fail');
   }
   res.json(req.body);
+});
+
+app.get('/welcome', (req, res, next) => {
+  res.render('welcome', {
+    username: req.cookies.username,
+  });
+});
+
+app.get('/logout', (req, res, next) => {
+  res.clearCookie('username');
+  res.redirect('/login');
 });
 
 const port = process.env.PORT;
