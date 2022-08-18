5cd908f9ebc2a333a12d5243ff974fdae272cfd3
JaimeGSandoval <jaime.devs@gmail.com>
2022-08-17 19:36:12 -0700

    Add first endpoint for planets

---
+++ NASA-project/server/src/routes/planets/planets.router.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const { getAllPlanets } = require('../../routes/planets/planets.controller');
+
+const planetsRouter = express.Router();
+planetsRouter.get('/planets', getAllPlanets);
+
+module.exports = planetsRouter;
