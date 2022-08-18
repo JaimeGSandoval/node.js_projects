4e1199fb0246cd7a23fc7a57f39f51846cf9c2f4
JaimeGSandoval <jaime.devs@gmail.com>
2022-08-18 14:24:22 -0700

    Refactor models & controllers for SoC

--- NASA-project/server/src/routes/planets/planets.router.js
+++ NASA-project/server/src/routes/planets/planets.router.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const { getAllPlanets } = require('../../routes/planets/planets.controller');
+const {
+  httpGetAllPlanets,
+} = require('../../routes/planets/planets.controller');
 
 const planetsRouter = express.Router();
-planetsRouter.get('/planets', getAllPlanets);
+planetsRouter.get('/planets', httpGetAllPlanets);
 
 module.exports = planetsRouter;
