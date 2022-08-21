817b901f72294e0c5b655398d5f6728e40e340eb
JaimeGSandoval <jaime.devs@gmail.com>
2022-08-20 22:23:26 -0700

    Add mongoose DB and env paswords

--- natours-api/server.js
+++ natours-api/server.js
@@ -1,6 +1,28 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 dotenv.config({ path: './config.env' });
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful'));
+
+// for local database connection
+// mongoose
+//   .connect(process.env.DATABASE_LOCAL, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//   })
+//   .then(() => console.log('Local DB connection successful'));
 
 const app = require('./app');
 
