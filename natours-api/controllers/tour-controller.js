1b7e2165bfdfe79d5b42d4e991f5d6e46f92bdcc
JaimeGSandoval <jaime.devs@gmail.com>
2022-08-21 19:43:54 -0700

    Add filtering and sorting of DB data from url queries

--- natours-api/controllers/tour-controller.js
+++ natours-api/controllers/tour-controller.js
@@ -18,8 +18,38 @@ const Tour = require('../models/tour-model');
 
 exports.getAllTours = async (req, res) => {
   try {
-    // find() will return an array of all the tours and convert them into js objects
-    const tours = await Tour.find();
+    console.log(req.query);
+    // BUILD QUERY
+    // 1a) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((field) => delete queryObj[field]);
+    // const { page, sort, limit, fields, ...queryObj } = req.query;
+
+    // 1b) Advanced Filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+
+    // this will return a query object that'll have all the Query methods we need to in order to be able to use the methods below i.e where(), equal(), so we can sort, do pagination etc.
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // using mongoose methods
+    // const query = await Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
+
+    // EXECUTE QUERY - since we use await here, the returned value will be the documents from the DB, which wouldn't have the Query methods on them
+    const tours = await query;
 
     res.status(200).json({
       status: 'success',
