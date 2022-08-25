9b5a2b7b023a57166931482afbc7b6f8d1a5bd75
JaimeGSandoval <jaime.devs@gmail.com>
2022-08-25 12:28:05 -0700

    update .env file & gitignore

--- NASA-project/server/src/services/mongo.js
+++ NASA-project/server/src/services/mongo.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
-const MONGO_URL =
-  'mongodb+srv://nasa-api:2CkS8iksyA8RySU3@nasacluster.rhe0mjh.mongodb.net/nasa?retryWrites=true&w=majority';
+require('dotenv').config();
+
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose.connection.once('open', () => {
   console.log('MongoDB connection ready');
