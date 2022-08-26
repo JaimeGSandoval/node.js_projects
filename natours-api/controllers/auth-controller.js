47e17421450b8e721ec71629be5e303171a8d6a7
JaimeGSandoval <jaime.devs@gmail.com>
2022-08-25 22:48:15 -0700

    Add sign up and login routes with jwt

--- natours-api/controllers/auth-controller.js
+++ natours-api/controllers/auth-controller.js
@@ -1,6 +1,13 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user-model');
 const catchAsync = require('../utils/catch-async');
+const AppError = require('../utils/app-error');
+
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    // the token will be valid for 90 days, then it will be expired
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create({
@@ -13,14 +20,36 @@ exports.signup = catchAsync(async (req, res, next) => {
   // CREATE JSONWEBTOKEN FOR AUTHENTICATION & SEND BACK TO USER
   // newUser._id is the id that's given to the user from mongodb
   // the value for JWT_SECRET should be 32 characters long
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    // the token will be valid for 90 days, then it will be expired
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
+  // we send back the token to log the user in as a result of signing up
   res.status(201).json({
     status: 'success',
     token,
     user: newUser,
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+
+  // 2) Check if user exists & password is correct
+  // use +password to include password because it's  not automatically being sent back from the DB to the server
+  const user = await User.findOne({ email }).select('+password');
+
+  // 3) If everything is ok, send token to client
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password', 401));
+  }
+
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 });
--- natours-api/models/user-model.js
+++ natours-api/models/user-model.js
@@ -20,6 +20,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minlength: 8,
+    select: false, // keeps password from being sent back from DB to server
   },
   passwordConfirm: {
     type: String,
@@ -36,11 +37,14 @@ const userSchema = new mongoose.Schema({
 });
 
 // USE BCRYPT TO HASH USER'S PASSWORD
+// before saving the document to the DB, we hash the password with bcrypt. This happens in the middle of receiving the data from the user and when we save the data to the DB
 userSchema.pre('save', async function (next) {
-  // only run this password if function was modified
+  // only runs if the password is new or if its been changed. i.e. if the user is only updating the email, we don't want to encrypt the password again. this.isModified checks the current document/user's field that we check has been modified. So if the password has not been modified .i.e the user isn't
+
+  // Whenever you change the value of the certain field, the value of isModified will get changed. So user.isModified('password') will be true whenever 'password' gets modified. In the first case, the password value initially would have been empty or null, so it will return true in that case as well.
   if (!this.isModified('password')) return next();
 
-  // hash user password with salt of 12
+  // if we make it down here a  hash hasn't been created for the password yet, so create hash for user password with salt of 12
   this.password = await bcrypt.hash(this.password, 12);
 
   // delete value for passwordConfirmed field. Setting to undefined means it won't be included with the data being sent back, too
@@ -48,6 +52,14 @@ userSchema.pre('save', async function (next) {
 
   next();
 });
+
+// the instance method correctPassword will be available to all instances of User
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
